5ba3692a79ffbac63c7070d21954a4bc595ad7cb
dariuszszymczak <dariusz.szymczak96@gmail.com>
2022-12-12 14:21:44 +0100

    initialize

--- src/components/layout/NavBar/NavBar.tsx
+++
@@ -1,5 +0,0 @@
-import styles from './NavBar.module.scss';
-export const NavBar: React.VoidFunctionComponent = () => {
-    return <div className={styles.navBar}>pies</div>;
-};
-export default NavBar;
--- src/views/MainView.tsx
+++
@@ -1,7 +0,0 @@
-import NavBar from '@components/layout/NavBar';
-export const MainView: React.VoidFunctionComponent = () => {
-    return <div id="main-box">
-        <NavBar />
-    </div>;
-};
-export default MainView;
